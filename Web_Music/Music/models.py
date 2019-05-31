62a91809f79e68bdeeadbf0ec973b19531d80436
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-05-31 23:48:50 +0000

    added a Http404 error page and details page

--- Web_Music/Music/models.py
+++ Web_Music/Music/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Album(models.Model):
+
     artist = models.CharField(max_length = 250)
     album_title = models.CharField(max_length = 500)
     genre = models.CharField(max_length = 100)
@@ -10,6 +11,7 @@ class Album(models.Model):
         return self.album_title + ' - ' + self.artist
 
 class Song(models.Model):
+    
     album = models.ForeignKey(Album,on_delete = models.CASCADE)
     file_type = models.CharField(max_length = 20)
     song_title = models.CharField(max_length = 100)
--- Web_Music/Music/views.py
+++ Web_Music/Music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from .models import Album,Song
 
 def index(request):
@@ -10,4 +10,10 @@ def index(request):
     return render(request, 'music/index.html', context)
 
 def details(request,album_id):
-    return HttpResponse('<h2> Details for Album id : ' + str(album_id) + '</h2>')
+    try:
+        album = Album.objects.get(pk = album_id)
+    
+    except Album.DoesNotExist : 
+        raise Http404("Album does not exist")
+
+    return render(request, 'music/details.html', {'album':album})  #another way of ding the dictionery work
