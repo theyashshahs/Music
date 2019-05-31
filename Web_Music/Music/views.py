7044802f61abd6a9ebaf9fda9041f467b48c3970
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-05-31 22:43:42 +0000

    Connected Database to templates

--- Web_Music/Music/views.py
+++ Web_Music/Music/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Album,Song
 
 def index(request):
-    return HttpResponse("<p>Hello Yash here</p>")
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = '/Music/' + str(album.id) + '/'
+        html += '<a href = " ' + url +  '" > ' + album.album_title + ' </a><br> '
+
+    return HttpResponse(html)
 
 def details(request,album_id):
     return HttpResponse('<h2> Details for Album id : ' + str(album_id) + '</h2>')
