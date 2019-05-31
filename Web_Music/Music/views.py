6c1890ed4b00ae488823935725bbe6886594fd6f
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-05-31 23:28:27 +0000

    Removed linking webpages bug

--- Web_Music/Music/views.py
+++ Web_Music/Music/views.py
@@ -4,12 +4,10 @@ from .models import Album,Song
 
 def index(request):
     all_albums = Album.objects.all()
-    html = ''
-    for album in all_albums:
-        url = '/Music/' + str(album.id) + '/'
-        html += '<a href = " ' + url +  '" > ' + album.album_title + ' </a><br> '
-
-    return HttpResponse(html)
+    context = {
+        'all_albums':all_albums,
+    }
+    return render(request, 'music/index.html', context)
 
 def details(request,album_id):
     return HttpResponse('<h2> Details for Album id : ' + str(album_id) + '</h2>')
