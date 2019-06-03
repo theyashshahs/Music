d1ed794bd78df873890afd0327ff951edbe11141
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-06-03 20:25:37 +0000

    Added sond objects

--- Web_Music/Music/models.py
+++ Web_Music/Music/models.py
@@ -15,3 +15,7 @@ class Song(models.Model):
     album = models.ForeignKey(Album,on_delete = models.CASCADE)
     file_type = models.CharField(max_length = 20)
     song_title = models.CharField(max_length = 100)
+
+    def __str__(self):
+       return self.song_title
+   
