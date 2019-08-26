16ba2a6e870007ff5e786c4448b8cad860fd25e3
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-08-26 18:38:33 +0530

    changed database and urls

--- Web_Music/Music/urls.py
+++ Web_Music/Music/urls.py
@@ -1,8 +1,17 @@
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index , name = 'index'),
-    #music/grouping-objects
-    url(r'^(?P<album_id>[0-9]+)/', views.details, name = 'details'),     
+   url(r'^$', views.index , name = 'index'),
+   
+   url(r'^(?P<album_id>[0-9]+)/', views.details, name = 'details'),     
 ]
+
+#urlpatterns = [
+#    path('', views.index, name = 'index'),
+#     #music/grouping-objects
+#    path('(?P<album_id>[0-9]+)/', views.details, name = 'details'),    
+
+#]
+
