ea341506ef5c76463b2fe2f98f011a40e882eed7
Yash Shah <42682370+Optimus28@users.noreply.github.com>
2019-09-06 22:27:42 +0530

    Jinja comment added in details template

--- Web_Music/Music/urls.py
+++ Web_Music/Music/urls.py
@@ -7,7 +7,7 @@ app_name = 'music'
 urlpatterns = [
    url(r'^$', views.index , name = 'index'), 
    url(r'^(?P<album_id>[0-9]+)/', views.details, name = 'details'),     
-   url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name = 'favourite'),
+   #url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name = 'favourite'),
 ]
 
 #urlpatterns = [
