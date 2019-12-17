8a27429c69a0e561c076e16c9e2d3680f965aaa5
yashshah2820 <201851150@iiitvadodara.ac.in>
2019-12-17 22:54:21 +0530

    rearranging templates and static directories

--- Web_Music/Music/urls.py
+++ Web_Music/Music/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import url
 from django.urls import path
-from Music import views
+from . import views
 
 app_name = 'music'
 
 urlpatterns = [
-   url(r'^$', views.index , name='index'),  # landing page
+   url(r'^$', views.index, name='index'),  # landing page
    url(r'^(?P<album_id>[0-9]+)/$', views.details, name='details'),     # details of the songs
    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name='favourite'),  # favourite function
 ]
--- Web_Music/Music/views.py
+++ Web_Music/Music/views.py
@@ -1,17 +1,17 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render, get_object_or_404
 from .models import Album, Song
 
 
 def index(request):
     all_albums = Album.objects.all()
     context = {
-        'all_albums':all_albums,
+        'all_albums': all_albums,
     }
     return render(request, 'music/index.html', context)
 
 
-def details(request,album_id):
-    album = get_object_or_404(Album, pk = album_id)  # replaces entire try and catch
+def details(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)  # replaces entire try and catch
     return render(request, 'music/details.html', {'album': album})  # another way of doing the dictionary work
 
 
